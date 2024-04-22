2e153a7c0da80a9d234554123929755385ec7a19
Huamagod1 <137719719+Huamagod1@users.noreply.github.com>
2024-04-22 19:49:28 -0400

    Ejericicio 5 Resuelto

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 import os
 os.system("cls")
 
-while True:
-    try:
-       
-      numero = int(input("Ingrese un valor numerico"))
-      if numero % 2 ==0:
-          
-         inpar = 2*numero+1
 
-    except:
-        print("Ingrese una wea valida porfa ")
+while True:
+             numero = int(input("Ingrese un valor numerico\n"))
+             if numero % 2 ==0:
+                 print("Favor ingrese un numero par")
+                 continue
+             else:
+               numero = numero*4
+               print(f"El numero inpar multiplicado por 4 es {numero}")
+               break    
         
