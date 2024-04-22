ae279849815f793a0772d9651c2a5f44d1d1483b
Huamagod1 <137719719+Huamagod1@users.noreply.github.com>
2024-04-22 19:26:05 -0400

    Avance ejercicio 5 numero par

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 #Aqui van las actividades generales de la 1 a la 6
+import os
+os.system("cls")
+
+while True:
+    try:
+       
+      numero = int(input("Ingrese un valor numerico"))
+      if numero % 2 ==0:
+          
+         inpar = 2*numero+1
+
+    except:
+        print("Ingrese una wea valida porfa ")
+        
