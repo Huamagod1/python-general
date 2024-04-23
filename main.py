36311bf8184ad3c5031fb8879144ca63726d6b38
Huamagod1 <137719719+Huamagod1@users.noreply.github.com>
2024-04-22 21:08:06 -0400

    A la mierda el numero primo

--- main.py
+++ main.py
@@ -14,12 +14,24 @@ os.system("cls")
 #                break    
 
 #Ejercicio 6
-resultado =1       
-base= int(input("Favor ingresar valor de la base\n"))
-exponente = int(input("Favor ingresar valor del exponente\n"))
+# resultado =1       
+# base= int(input("Favor ingresar valor de la base\n"))
+# exponente = int(input("Favor ingresar valor del exponente\n"))
 
-for i in range(0,exponente):
-   resultado  = resultado * base  
+# for i in range(0,exponente):
+#    resultado  = resultado * base  
 
-print(f"el resultado {base} con exponente {exponente} es de: {resultado}")
+# print(f"el resultado {base} con exponente {exponente} es de: {resultado}")
 
+#Ejercicio 7
+
+numero = int(input("Ingrese un numero\n"))
+
+if numero == 1:
+  print("No es un numero primero")
+else:
+  for i in range(2,numero):
+    if numero % i == 0:
+      print("No es un numero primo")
+    else:
+      print("ES un numero Primo")
