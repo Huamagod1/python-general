53e5a7fecafe1bd27fd0e338271c0e2dfc8d28e7
Huamagod1 <137719719+Huamagod1@users.noreply.github.com>
2024-04-22 20:11:14 -0400

    Ejercicio 6 resuelto

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ import os
 os.system("cls")
 
 
-while True:
-             numero = int(input("Ingrese un valor numerico\n"))
-             if numero % 2 ==0:
-                 print("Favor ingrese un numero par")
-                 continue
-             else:
-               numero = numero*4
-               print(f"El numero inpar multiplicado por 4 es {numero}")
-               break    
-        
+# while True:
+#              numero = int(input("Ingrese un valor numerico\n"))
+#              if numero % 2 ==0:
+#                  print("Favor ingrese un numero par")
+#                  continue
+#              else:
+#                numero = numero*4
+#                print(f"El numero inpar multiplicado por 4 es {numero}")
+#                break    
+
+#Ejercicio 6
+resultado =1       
+base= int(input("Favor ingresar valor de la base\n"))
+exponente = int(input("Favor ingresar valor del exponente\n"))
+
+for i in range(0,exponente):
+   resultado  = resultado * base  
+
+print(f"el resultado {base} con exponente {exponente} es de: {resultado}")
+
